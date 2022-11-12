9ad9d5b0961cfbf36f9a5a7dbd2d6e840cb81ae8
Vishal <vp2264613@gmail.com>
2022-11-11 23:44:14 -0500

    commit4

--- Full-Stack-II/Lab/Exercise-1/myapp/routes/users.js
+++ Full-Stack-II/Lab/Exercise-1/myapp/routes/users.js
@@ -1,15 +1,12 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var router = express.Router();
+var bodyParser = require("body-parser");
 
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({ extended: this.true }));
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-router.post("/users", function (req, res, next) {
-  console.log("First name: Test Last name: User");
-  res.send("This is a post request");
-});
 module.exports = router;
